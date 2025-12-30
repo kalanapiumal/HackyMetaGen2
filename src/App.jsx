1b9615c07ca2529032e6b7f8e93fbd5d7c0cf7c7
Kalana Piumal <kalanapiumal2@gmail.com>
2025-12-31 04:57:12 +0530

    Update App.jsx9

--- src/App.jsx
+++ src/App.jsx
@@ -331,7 +331,7 @@ const HackyMetaGenApp = () => {
       contentParts.push({ inlineData: { mimeType, data: base64Data } });
     }
 
-    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${activeKey}`, {
+    const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-09-2025:generateContent?key=${activeKey}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
